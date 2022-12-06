bb2900f32831f7ab3eab4b6145fed9c55800fa01
Joshua Rubin <me@jawa.dev>
2022-12-05 23:05:33 -0700

    latest updates
    Signed-off-by: Joshua Rubin <me@jawa.dev>

--- modules/systemd.nix
+++ modules/systemd.nix
@@ -42,8 +42,27 @@
       };
 
       Install = {
-        WantedBy = ["multi-user.target"];
+        WantedBy = ["default.target"];
+      };
+    };
+
+    et = {
+      Unit = {
+        Description = "Eternal Terminal";
+        After = ["syslog.target" "network.target"];
+      };
+
+      Service = {
+        Type = "simple";
+        Restart = "on-failure";
+        ExecStart = "${pkgs.eternal-terminal}/bin/etserver --cfgfile=${sysConfig.homeDirectory}/.config/et/et.cfg";
+      };
+
+      Install = {
+        WantedBy = ["default.target"];
       };
     };
   };
+
+  xdg.configFile."et/et.cfg".source = ../files/config/et.cfg;
 }
