8c9cd8678ff5646cf5c1a1e9ab2f68be25cc7f51
Joshua Rubin <me@jawa.dev>
2023-02-22 14:38:54 -0700

    latest updates
    Signed-off-by: Joshua Rubin <me@jawa.dev>

--- overlays/local.nix
+++ overlays/local.nix
@@ -1,22 +1,2 @@
 self: super: {
-  infra = super.callPackage "${super.path}/pkgs/tools/admin/infra" {
-    buildGoModule = old:
-      super.buildGoModule (
-        old
-        // rec {
-          version = "0.19.0";
-          src = super.fetchFromGitHub {
-            owner = "infrahq";
-            repo = old.pname;
-            rev = "v${version}";
-            sha256 = "sha256-eQG+Vi5FfV64iEIYQ3Djd12TfdaLOVrfRxgN5ESJ4nA=";
-          };
-          vendorSha256 = "sha256-Xy8dAalj/NweZHEo2IpW+L+YzmBgYD+EU7ZKOhh3iIY=";
-        }
-      );
-  };
-
-  buf = super.buf.overrideAttrs (_old: rec {
-    doCheck = false;
-  });
 }
