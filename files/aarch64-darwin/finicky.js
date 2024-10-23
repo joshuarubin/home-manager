fcab1b264f254952add7587f711468e5fb1fd2cf
Joshua Rubin <me@jawa.dev>
2024-10-23 15:49:55 -0600

    latest updates
    Signed-off-by: Joshua Rubin <me@jawa.dev>

---
+++ files/aarch64-darwin/finicky.js
@@ -0,0 +1,132 @@
+const browsers = {
+  arc: {
+    name: "Arc",
+    openInBackground: false,
+  },
+  finder: {
+    name: "Finder",
+    openInBackground: false,
+  },
+  zoom: {
+    name: "us.zoom.xos",
+    openInBackground: false,
+  },
+};
+
+module.exports = {
+  defaultBrowser: browsers.arc,
+  options: {
+    hideIcon: false,
+  },
+  rewrite: [
+    {
+      // Remove all marketing/tracking information from urls
+      match: () => true,
+      url: ({ url }) => {
+        // Remove all query parameters beginning with these strings
+        const removeKeysStartingWith = ["utm_", "uta_"];
+
+        // Remove all query parameters matching these keys
+        const removeKeys = ["fbclid", "gclid"];
+
+        const search = url.search
+          .split("&")
+          .map((parameter) => parameter.split("="))
+          .filter(
+            ([key]) =>
+              !removeKeysStartingWith.some((startingWith) =>
+                key.startsWith(startingWith),
+              ),
+          )
+          .filter(
+            ([key]) => !removeKeys.some((removeKey) => key === removeKey),
+          );
+
+        return {
+          ...url,
+          search: search.map((parameter) => parameter.join("=")).join("&"),
+        };
+      },
+    },
+    {
+      // Open zoom urls in the Zoom app
+      match: ({ url }) =>
+        url.host.includes("zoom.us") && url.pathname.includes("/j/"),
+      url: ({ url }) => {
+        try {
+          var pass = "&pwd=" + url.search.match(/pwd=(\w*)/)[1];
+        } catch {
+          var pass = "";
+        }
+        var conf = "confno=" + url.pathname.match(/\/j\/(\d+)/)[1];
+        return {
+          search: conf + pass,
+          pathname: "/join",
+          protocol: "zoommtg",
+        };
+      },
+    },
+    {
+      // Open linear urls in the Linear app
+      match: ({ url }) => url.host.match(/^(\w+\.)?linear\.app$/),
+      url: ({ url }) => {
+        if (url.host.match(/^(\w+\.)?uploads\.linear\.app$/)) {
+          return url;
+        }
+        return {
+          ...url,
+          protocol: "linear",
+        };
+      },
+    },
+    {
+      // Append `opener_=slack` to all urls opened from Slack so Arc Air Traffic
+      // Control can match on them
+      match: ({ opener }) => opener.bundleId === "com.tinyspeck.slackmacgap",
+      url: ({ url }) => {
+        const search = url.search
+          .split("&")
+          .filter((r) => r !== "")
+          .concat(["opener_=slack"])
+          .join("&");
+
+        return { ...url, search };
+      },
+    },
+    {
+      match: ({ url }) => url.protocol === "s3",
+      url: ({ urlString, url }) => {
+        if (urlString.search(/post-training-datasets.*parquet/) != -1) {
+          return {
+            protocol: "https",
+            host: "podium.poolsi.de",
+            pathname: `/dataset_viewer`,
+            search: `dataset_path=${urlString}`,
+          };
+        }
+
+        const bucket = url.host;
+        let path = url.pathname.substring(1); // Remove the leading slash
+
+        return {
+          protocol: "https",
+          host: "us-east-2.console.aws.amazon.com",
+          pathname: `/s3/buckets/${bucket}`,
+          search: `prefix=${path}`,
+        };
+      },
+    },
+  ],
+  handlers: [
+    {
+      // Open zoom urls in the Zoom app
+      match: /zoom\.us\/join/,
+      browser: browsers.zoom,
+    },
+    {
+      // Open linear urls in the Linear app
+      match: ({ url }) => url.protocol === "linear",
+      browser: browsers.finder,
+    },
+  ],
+};
--- modules/aarch64-darwin/files.nix
+++ modules/aarch64-darwin/files.nix
@@ -1,5 +1,6 @@
 _: {
   home.file = {
+    ".finicky.js".source = ../../files/aarch64-darwin/finicky.js;
     ".ssh/config".source = ../../files/ssh/config;
     ".ssh/connections/.keep".text = "";
     "Library/KeyBindings/DefaultKeyBinding.dict".source = ../../files/aarch64-darwin/Library/KeyBindings/DefaultKeyBinding.dict;
